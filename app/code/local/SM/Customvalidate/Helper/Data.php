b76ccbc24d29e8e09ab28e2478b204d1f5e4657e
quangdv <quangdv02@gmail.com>
2015-08-02 23:24:46 +0700

    validate

---
+++ app/code/local/SM/Customvalidate/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Computer
+ * Date: 8/2/2015
+ * Time: 10:58 PM
+ */ 
+class SM_Customvalidate_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ skin/frontend/rwd/default/js/sm/custom_validate.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Computer on 8/2/2015.
+ */
+Validation.add('validate-email-exist', 'abc xyz', function(v) {
+    return v == 1;
+});
